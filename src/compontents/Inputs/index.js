cf061607fe22367e8b9c0ae7724b45b05ed761b3
Higor Rabelo <higor.rabelo@gmail.com>
2023-08-23 19:06:30 -0300

    Correção de posicionamentos do layout 2

--- src/compontents/Inputs/index.js
+++ src/compontents/Inputs/index.js
@@ -4,7 +4,7 @@ export function Inputs(props){
     return(
      <div>
         <label className="labels">{props.nome}</label>
-        <input className="inputs" type="text" placeholder="" />
+        <input className="inputs" type={props.nome=='Senha'?'password':'text'} placeholder="" />
      </div>
     );
 }
